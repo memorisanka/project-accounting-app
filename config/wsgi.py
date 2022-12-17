3c1b062406bf0c602e30cdf6b3769832a82b8f7f
xc4patryk <xc.patryk@gmail.com>
2022-12-17 14:16:57 +0100

    Rename application to "config"

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI project_accounting_app for project_accounting_app project.
+WSGI config for project_accounting_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_accounting_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
