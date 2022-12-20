e1f1d56f9e33e53a133d8a487ddab13adb380149
memorisanka <j.krawczyk87@gmail.com>
2022-12-20 21:09:32 +0100

    fix: delete migrations in core app

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import Product
 
-
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'unit', 'steel_type', 'amount')
 
