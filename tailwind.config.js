e38567b8bdb6076320f362e52b796d32ea0066f9
xc4patryk <xc.patryk@gmail.com>
2022-12-29 02:39:55 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './templates/**/*.html'
+      './templates/**/*.html',
+      './users/templates/**/*.html'
   ],
   theme: {
     extend: {},
