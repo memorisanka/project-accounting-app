3860cd21567a18ba573d530a2fbcc8087cac8f54
memorisanka <j.krawczyk87@gmail.com>
2023-01-04 19:51:41 +0100

    feat(projects/models): add client field to Project model

--- projects/admin.py
+++ projects/admin.py
@@ -1,17 +1,35 @@
 from django.contrib import admin
-from .models import Product, Project, Worker
+from .models import Product, Project, Worker, Client
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "amount", "unit", "steel_type", )
+    list_display = (
+        "name",
+        "amount",
+        "unit",
+        "steel_type",
+    )
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("name", )
+    list_display = ("name",)
 
 
 @admin.register(Worker)
 class WorkerAdmin(admin.ModelAdmin):
-    list_display = ("name", )
+    list_display = (
+        "name",
+        "surname",
+    )
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "address",
+        "contact_person",
+        "phone",
+        "email",
+    )
--- projects/models/project.py
+++ projects/models/project.py
@@ -1,7 +1,9 @@
 from django.db import models
+from .client import Client
 from .worker import Worker
 
 
 class Project(models.Model):
     name = models.CharField(max_length=50)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, default=None)
     workers = models.ManyToManyField(Worker, related_name='projects')
