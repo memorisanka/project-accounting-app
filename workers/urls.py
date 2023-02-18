c38c8b57ae8a7d3a1087cf45d07481f0af0dd4aa
memorisanka <j.krawczyk87@gmail.com>
2023-02-18 20:03:07 +0100

    feat(workers): add deleting and updating worker

--- workers/urls.py
+++ workers/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('worker/<int:pk>/', views.DetailWorkerView.as_view(), name='detail-worker'),
     path('worker/create/', views.CreateWorkerView.as_view(), name='create-worker'),
     path('worker/list/', WorkerListView.as_view(), name="workers-list"),
-    # path('worker/<int:pk>/update/', views.UpdateClientView.as_view(), name='update-client'),
-    # path('worker/<int:pk>/delete/', views.DeleteClientView.as_view(), name='delete-client'),
+    path('worker/<int:pk>/update/', views.UpdateWorkerView.as_view(), name='update-worker'),
+    path('worker/<int:pk>/delete/', views.DeleteWorkerView.as_view(), name='delete-worker'),
 ]
